2a7e7252c1b61a12cd1d6b26980e221ad5b63101
Cufeed <gusman3000@mail.ru>
2025-03-08 19:56:29 +0300

    fix_format_4

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,10 @@
 // Copyright 2025 UNN-CS Team
 
-#include "circle.h"
-#include "tasks.h"
 #include <gtest/gtest.h>
 #include <corecrt_math_defines.h>
 #include <cstdint>
+#include "circle.h"
+#include "tasks.h"
 
 
 TEST(CircleTest, ConstructorInitialization) {
